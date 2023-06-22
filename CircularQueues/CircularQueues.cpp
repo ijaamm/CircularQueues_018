3e178293f4c2a0b4e414a5852331600d386d7f35
ijaamm <ijammusyaf@gmail.com>
2023-06-22 10:57:40 +0700

    COMMIT 11

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -44,5 +44,17 @@ public:
 			return;
 		}
 		cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
+		//cek jika antrian hanya memiliki 1 element 
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika element yang di hapus berada di posisi terakhir array kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 };
