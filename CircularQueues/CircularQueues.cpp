7c2e09985cdc5b06dfc327972894e1d481a49e23
ijaamm <ijammusyaf@gmail.com>
2023-06-22 11:27:00 +0700

    commit 21

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -111,6 +111,29 @@ int main() {
 			cout << "enter your choise (1-4): ";
 			cin >> ch;
 			cout << endl;
+
+			switch (ch) {
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
 		}
+		
 	}
 }
